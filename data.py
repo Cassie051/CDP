4fc9242264ae1a205013278194a454b47337e99d
Cassie051 <kasiaczarna051@gmail.com>
2020-03-29 21:31:04 +0200

    Add lt to Row class

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # open and red file
-f = open('dane/data20.txt')
+f = open('dane/data200.txt')
 first_line = " ".join(f.readline().split())
 
 # added parameters from file
@@ -16,8 +16,8 @@ class Row:
     self.p = p
     self.q = q
 
-  # def __lt__(self, other):
-  #   return self.r < other.r
+  def __lt__(self, other):
+    return self.r < other.r
 
 
 # added data to row
