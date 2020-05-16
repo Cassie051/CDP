5888f85a79181febdd595132c914a8c08d898c71
Katarzyna Czarnacka <kasiaczarna051@gmail.com>
2020-05-16 18:46:29 +0200

    works Johnson

--- task3/lab7.py
+++ task3/lab7.py
@@ -7,28 +7,28 @@ M = data.m
 
 def Johnson(jProcesses):
     l = 0
-    k = N
+    k = N - 1
     minP = int(sys.maxsize)
-    result = [0]*(k-1)
+    result = [[0 for i in range(2)] for j in range(k+1)]
     minN = 0
 
     while (len(jProcesses)!= 0):
-        for i in range(0, len(jProcesses)-1):
+        for i in range(0, len(jProcesses)):
             for j in range(0, M):
                 minP = min(jProcesses[i][j], minP)
                 if(minP == jProcesses[i][j]):
                     minN = i
                     minM = j
         if (jProcesses[minN][0] < jProcesses[minN][1]):
-            result.insert(l, minP)
+            result[l] = jProcesses[minN]
             l += 1
             minP = int(sys.maxsize)
         else:
-            result.insert(k, minP)
+            result[k] = jProcesses[minN]
             k -= 1
             minP = int(sys.maxsize)
         jProcesses.remove(jProcesses[minN])
-
+    # Cmax(result)
     return result
 
 print(Processes[0][0])
