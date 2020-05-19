8df66946defb6db771c4eff31469da4b56522e00
Katarzyna Czarnacka <kasiaczarna051@gmail.com>
2020-05-19 18:14:53 +0200

    too big

--- task3/lab8.py
+++ task3/lab8.py
@@ -12,49 +12,66 @@ def Bound(BProcesses, pi, case):
     pall = 0
     if(case == 1):
         for i in range(0, M):
+            p = 0
             for j in range(0, len(BProcesses)-1):
                 p += BProcesses[j][i]
-                result = max(result, pi[len(pi)-1][i] + p)
+            result = max(result, pi[len(pi)-1][i] + p)
         return result
     elif(case == 2):
         pmin = int(sys.maxsize)
         for i in range(0, M):
-            for j in range(0, len(BProcesses)):
-                p += BProcesses[i][j]
+            for j in range(0, len(BProcesses)-1):
+                p += BProcesses[j][i]
                 for k in range(i+1, M):
-                    for g in range(0, N):
-                        pmin = min(pmin, BProcesses[k][g])
-                    pall += pmin
-                result = max(result, pi[len(pi)-1][i] + p + pall)
+                    pmin = min(pmin, BProcesses[j][k])
+                pall += pmin
+            result = max(result, pi[len(pi)-1][i] + p + pall)
         return result
     elif(case == 3):
+        pmin = int(sys.maxsize)
+        for i in range(0, M):
+            for j in range(0, len(BProcesses)):
+                p += BProcesses[j][i]
+                for k in range(i+1, M):
+                    pmin = min(pmin, Processes[j][k])
+                pall += pmin
+                result = max(result, pi[len(pi)-1][i] + p + pall)
         return result
     elif(case == 4):
+        pmin = int(sys.maxsize)
+        for i in range(0, M):
+            for j in range(0, len(BProcesses)):
+                p += BProcesses[j][i]
+                for k in range(i+1, M):
+                    pall += Processes[j][k]
+                pmin = min(pmin, pall)
+                result = max(result, pi[len(pi)-1][i] + p + pall)
         return result
     else:
         return print("No LB type")
 
-def BnB(j, BProcesses, pi):
-    UB = int(sys.maxsize)
+def BnB(j, BProcesses, pi, result):
+    UB = result
+    pi = pi.copy()
     pi.append(j)
     BProcesses.remove(j)
     if not (len(BProcesses) == 0):
         LB =  Bound(BProcesses, pi, 1)
         if(LB <= UB):
             for j in BProcesses:
-                BnB(j, BProcesses, pi)
+                UB = BnB(j, BProcesses, pi, UB)
     else:
         Cmax =  lab6.Cmax(pi)
         if(Cmax < UB):
             UB = Cmax
-            best = pi         # zapamietujemy najlepsza permutacja
     return UB
 
 
 def initBnB(BProcesses):
     pi = []
-    pern = []
+    result = int(sys.maxsize)
     for j in BProcesses:
-        return BnB(j, BProcesses, pi)
+        result = min(result, BnB(j, BProcesses, pi, result))
+    return result
 
 print(initBnB(Processes))
