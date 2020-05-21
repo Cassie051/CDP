a350c5d2ba90f96fbc3e6c4d794b9dd8e09d047d
Katarzyna Czarnacka <kasiaczarna051@gmail.com>
2020-05-21 12:26:47 +0200

    Neh init

---
+++ task4/data.py
@@ -0,0 +1,27 @@
+filepath = 'D:/Git/CDP/task4/dane/ta001.txt'
+
+with open(filepath) as fp:
+    first_line = " ".join(fp.readline().split())
+    first_second = " ".join(fp.readline().split())
+
+    # added parameters from file
+    NMfirst_line = first_second.split()
+    n = int(NMfirst_line[0])
+    m = int(NMfirst_line[1])
+
+    tmp = []
+    hel = []
+    for i in range (n):
+        hel.append([int(m) for m in fp.readline().split()])
+
+    for i in range (n):
+        new =[]
+        for j in range(1, len(hel[0]), 2):
+            new.append(hel[i][j])
+        tmp.append(new)
+
+data = [[0 for i in range(n)] for j in range(m)]
+
+for i in range(n):
+    for j in range(m):
+        data[j][i] = tmp[i][j]
---
+++ task4/lab9.py
@@ -0,0 +1,28 @@
+import data
+
+Processes = data.data.copy()
+N = data.n
+M = data.m
+
+def Cmax(CProcesses):
+    Cstart = 0
+    Cend = 0
+    endList = [[0 for i in range(N)]  for j in range(M)]
+    startList = [[0 for i in range(N)]  for j in range(M)]
+    for i in range(0, M):
+        for j in range(0, len(CProcesses[0])):
+            if(i == 0):
+                Cstart = Cend
+                Cend = Cstart + CProcesses[i][j]
+            else:
+                if(j == 0):
+                    Cstart = endList[i-1][j]
+                else:
+                    Cstart = max(endList[i-1][j], Cend)
+                Cend = Cstart + CProcesses[i][j]
+
+            startList[i][j] = Cstart
+            endList[i][j] = Cend
+    return endList[M-1][N-1]
+
+# def Neh(NProcesses):
