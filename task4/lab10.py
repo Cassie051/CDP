9562b0ae8971ab0a138bf4981cc7e6cb9cf26851
Foreseeker <49638998+Foreseeker@users.noreply.github.com>
2020-05-24 01:10:45 +0200

    Nearly done

--- task4/lab10.py
+++ task4/lab10.py
@@ -8,6 +8,12 @@ Processes = data.data.copy()
 N = data.n
 M = data.m
 
+T0 = 100 #100 10000
+L = math.sqrt(N) #N N*N
+x = T0/1000 #T0/10000 T0/100000
+alpha = 0,97 #0,95 0,90
+
+
 def Cmax(CProcesses):
     Cstart = 0
     Cend = 0
@@ -29,7 +35,7 @@ def Cmax(CProcesses):
             endList[i][j] = Cend
     return endList[M-1][N-1]
 
-#Kowalstwo
+#ChilleraUtopia
 
 def deltaCmac(pi, pi_new):
     return Cmax(pi)-Cmax(pi_new)
@@ -39,7 +45,7 @@ def probability(pi, pi_new, T):
     prob = math.exp(delta/T)
     return prob
 
-def chillLinear(T,x):
+def chillLinear(T, x):
     Ice_Cold_T = T - x
     return Ice_Cold_T
 
@@ -47,31 +53,46 @@ def chillGeometric(T, alpha):
     Ice_Cold_T = T*alpha
     return Ice_Cold_T
 
-def chillLog(T, iter):
-    Ice_Cold_T = T/math.log(iter+1)
+def chillLog(T, iterator):
+    Ice_Cold_T = T/math.log(iterator+1)
     return Ice_Cold_T
 
-def moveMethod(mm, i, j, pi):
+def moveMethod(mm, i, j, pi, AlgProc):
     if mm == 'swap':
-
+        #temp = AlgProc[pi]
+        #AlgProc[pi] = AlgProc[i][j]
+        #Algproc[i][j] = temp
+        #return AlgProc
     elif mm == 'insert':
-    
+        #AlgProc[pi] = AlgProc[i][j]
+        #return AlgProc
     elif mm == 'twist':
-        
+        #AlgProc[pi] = AlgProc[j][i]
+        #return AlgProc
     elif mm == 'adjacent':
-    
+        #return AlgProc
+    else:
+        pass
+
+def reduceMethod(rm, T, iterator):
+    if rm == 'linear':
+        chillLinear(T, x)
+    elif rm == 'geometric'
+        chillGeometric(T, alpha)
+    elif rm == 'log':
+        chillLog(T, iterator)
     else:
         pass
 
 def SAA():
-    T0 = sys.maxsize
+    AlgProc = Processes.copy()
     T = T0
     pi = 5
-    while T>Tend:
+    while T > Tend:
         for k = 1 in range(1,L):
             i = random.randint(1,N)
             j = random.randint(1,N)
-            pi_new = moveMethod(mm,i,j,pi)
+            pi_new = moveMethod(mm,i,j,pi, AlgProc)
             if Cmax(pi_new) > Cmax(pi):
                 r = random.random()
                 p = probability(pi, pi_new)
