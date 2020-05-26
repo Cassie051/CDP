dea437df6f4686c388c483d4ab4ccea0f04ecc8c
Foreseeker <49638998+Foreseeker@users.noreply.github.com>
2020-05-26 16:51:39 +0200

    Update in progress

--- task4/lab10.py
+++ task4/lab10.py
@@ -12,7 +12,7 @@ T0 = 100 #100 10000
 L = math.sqrt(N) #N N*N
 x = T0/1000 #T0/10000 T0/100000
 alpha = 0,97 #0,95 0,90
-
+Tend = 0.01 #0.001 0.0001
 
 def Cmax(CProcesses):
     Cstart = 0
@@ -57,20 +57,35 @@ def chillLog(T, iterator):
     Ice_Cold_T = T/math.log(iterator+1)
     return Ice_Cold_T
 
-def moveMethod(mm, i, j, pi, AlgProc):
+def moveMethod(mm, i, j, pi):
     if mm == 'swap':
-        #temp = AlgProc[pi]
-        #AlgProc[pi] = AlgProc[i][j]
-        #Algproc[i][j] = temp
-        #return AlgProc
+        for x in range (M):
+            pi[i][x],pi[j][x] = pi[j][x],pi[i][x] #prosta zamiana 
+        return pi
+
     elif mm == 'insert':
-        #AlgProc[pi] = AlgProc[i][j]
-        #return AlgProc
+        for x in range(M):
+            temp = pi[i][x]
+            for y in range(N):
+                if y < j and y < i: #elementy są przed wyciąganym i jego miejscem
+                    pass
+                elif y < j and y > i: #elemetny pomiędzy elementem wyciągniętym i jego miejscem
+                    pi[y][x] = pi [y-1][x]
+                elif y > j and y < i: #elementy pomiędzy miejscem i elementem wyciągniętym
+                    pi[y][x] = pi [y+1][x]
+                elif y > j and y > i: #elementy po miejscach interaktywych
+                    pass
+                else:
+                    pass
+        return pi
     elif mm == 'twist':
-        #AlgProc[pi] = AlgProc[j][i]
-        #return AlgProc
+        for x in range(M):
+            while i < j:
+                pi[i][x],pi[j][x] = pi[j][x],pi[i][x]
+
     elif mm == 'adjacent':
-        #return AlgProc
+        
+
     else:
         pass
 
@@ -87,19 +102,21 @@ def reduceMethod(rm, T, iterator):
 def SAA():
     AlgProc = Processes.copy()
     T = T0
-    pi = 5
+    pi = AlgProc
+    pi_best = pi
+    
     while T > Tend:
         for k = 1 in range(1,L):
             i = random.randint(1,N)
             j = random.randint(1,N)
-            pi_new = moveMethod(mm,i,j,pi, AlgProc)
+            pi_new = moveMethod(mm,i,j,pi)
             if Cmax(pi_new) > Cmax(pi):
                 r = random.random()
                 p = probability(pi, pi_new)
                 if r >= p:
                     pi_new = pi
             pi = pi_new
-            if Cmax(pi) < Cmax(pi_new):
+            if Cmax(pi) < Cmax(pi_best):
                 pi_new = pi
         T = reduceMethod(rm, T)
     return T
