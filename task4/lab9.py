7346ff7a903a32584b3572698eb9f33b75ba9d0c
Katarzyna Czarnacka <kasiaczarna051@gmail.com>
2020-05-28 00:42:45 +0200

    NehPlus not work

--- task4/lab9.py
+++ task4/lab9.py
@@ -26,6 +26,28 @@ def Cmax(CProcesses):
             endList[i][j] = Cend
     return endList[len(CProcesses)-1][len(CProcesses[0])-1]
 
+def CNeh(CProcesses):
+    Cstart = 0
+    Cend = 0
+    endList = [[0 for i in range(len(CProcesses[0]))]  for j in range(len(CProcesses))]
+    startList = [[0 for i in range(len(CProcesses[0]))]  for j in range(len(CProcesses))]
+    for i in range(0, len(CProcesses)):
+        for j in range(0, len(CProcesses[0])):
+            if(i == 0):
+                Cstart = Cend
+                Cend = Cstart + CProcesses[i][j]
+            else:
+                if(j == 0):
+                    Cstart = endList[i-1][j]
+                else:
+                    Cstart = max(endList[i-1][j], Cend)
+                Cend = Cstart + CProcesses[i][j]
+            startList[i][j] = Cstart
+            endList[i][j] = Cend
+
+    return endList
+
+
 def Neh(NProcesses):
     k = 0
     W = []
@@ -56,16 +78,39 @@ def Neh(NProcesses):
     return Cmax(pi)
 
 
-# def Select(W, case):
-#     if(case == 1):
-#         # najdluzsza operacja na sciezce krytycznej
-#     elif(case == 2):
-#         # najwieksza suma operacji wchodzacych na sciezke krytyczna
-#     elif(case == 3):
-#         # najwieksza liczba operacji wchodzacych na sciezke krytyczna
-#     elif(case == 4):
-#         # najwieksze zmniejszenie Cmax
-#         for
+def Select(Wx, pi, case):
+    if(case == 1):
+        # najdluzsza operacja na sciezce krytycznej
+        return "nothing"
+    elif(case == 2):
+        # najwieksza suma operacji wchodzacych na sciezke krytyczna
+        List = CNeh(pi)
+        for i in range(len(List)-1, 0):
+            for j in range(len(List[0])-1, 0):
+                tmp = List[i][j]
+    elif(case == 3):
+        # najwieksza liczba operacji wchodzacych na sciezke krytyczna
+        return "nothing"
+    elif(case == 4):
+        # najwieksze zmniejszenie Cmax
+        tpi = copy.deepcopy(pi)
+        elier = -2
+        ref = Cmax(pi)
+        tmptask = [0 for i in range(len(pi))]
+        for j in range(len(pi[0])):
+            for i in range(len(pi)):
+                tmptask[i] = pi[i][j]
+                del pi[i][j]
+            tmp = Cmax(pi)
+            if elier == -2:
+                elier = tmp
+                task = copy.deepcopy(tmptask)
+            elif ref-elier < ref-tmp:
+                task = copy.deepcopy(tmptask)
+            pi = copy.deepcopy(tpi)
+        return task
+
+
 
 def NehPlus(NProcesses):
     k = 0
@@ -73,6 +118,7 @@ def NehPlus(NProcesses):
     pi = [[] for j in range(M)]
     pitmp = [[] for j in range(M)]
     tabj = []
+    flag = -1
     for i in range(N):
         tmp = 0
         for j in NProcesses:
@@ -93,20 +139,23 @@ def NehPlus(NProcesses):
             if Cmax(pitmp) < Cmax(pi):
                 pi = copy.deepcopy(pitmp)
         del W[len(W)-1]
+        flag += 1
 
-        x = Select(W, 4)
-        if(x != j):
-            for i in range(len(x)):
-                pitmp[i].append(x[i])
-                pi[i].append(x[i])
-            for l in range(0, k):
-                if k>0:
-                    for i in range(len(x)):
-                        pitmp[i][k-l], pitmp[i][k-l-1] = pitmp[i][k-l-1], pitmp[i][k-l]
-                if Cmax(pitmp) < Cmax(pi):
-                    pi = copy.deepcopy(pitmp)
+        if(flag > 0):
+            Wx = copy.deepcopy(W)
+            x = Select(Wx, pi.copy(), 4)
+            if(x != j):
+                for i in range(len(x)):
+                    pitmp[i].append(x[i])
+                    pi[i].append(x[i])
+                for l in range(0, k):
+                    if k>0:
+                        for i in range(len(x)):
+                            pitmp[i][k-l], pitmp[i][k-l-1] = pitmp[i][k-l-1], pitmp[i][k-l]
+                    if Cmax(pitmp) < Cmax(pi):
+                        pi = copy.deepcopy(pitmp)
         k += 1
     return Cmax(pi)
 
 
-print(Neh(Processes))
+print(NehPlus(Processes))
